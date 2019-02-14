ee6838213cbaee9e5f20e36c5b6714b59d0febe1
TB831 <druu831@gmail.com>
2019-02-14 10:53:15 -0800

    boilerplate for Todos components

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
   <div id="app">
+    <Todos />
   </div>
 </template>
 
 <script>
+import Todos from './components/Todos.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Todo One',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Todo Two',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Todo Three',
+          completed: false
+        },
+      ]
+    }
   }
 }
 </script>
